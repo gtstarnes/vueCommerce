56126eeba3fa041836d4adc137c71265301f7da4
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-07-15 14:14:30 -0400

    initial create for CartCounter

---
+++ src/components/counter/CartCounter.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup></script>
+
+<template>
+    <div>
+        <button></button>
+        <span></span>
+        <button></button>
+    </div>
+</template>
+
+<style lang="css" scoped></style>
