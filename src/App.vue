d8f4f36a1ff3ff2f2d30a2cf7c033b5470fc6526
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-07-16 07:36:44 -0400

    add cart button for test

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import CartCounter from './components/counter/CartCounter.vue';
+import AddButton from './components/cartButtons/AddButton.vue'
 
 </script>
 
 <template>
   <CartCounter />
+  <AddButton />
 </template>
 
 <style scoped>
