e3dcf608258a3219e125d73fcb9dfa9f701afb5d
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-07-16 08:25:43 -0400

    intial create for item controls

---
+++ src/components/item/ItemControls.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import AddButton from '../cartButtons/AddButton.vue';
+import CartCounter from '../counter/CartCounter.vue';
+
+</script>
+
+<template>
+    <div>
+        <CartCounter />
+        <AddButton />
+    </div>
+
+</template>
+
+<style lang="css" scoped></style>
