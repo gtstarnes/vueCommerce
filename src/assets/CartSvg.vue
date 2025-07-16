66acbf4a4cca39160eb373eaf557a92ce60f46f5
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-07-16 07:38:18 -0400

    change size

--- src/assets/CartSvg.vue
+++ src/assets/CartSvg.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup></script>
 
 <template>
-    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24">
         <path fill="currentColor" d="M7 22q-.825 0-1.412-.587T5 20t.588-1.412T7 18t1.413.588T9 20t-.587 
             1.413T7 22m10 0q-.825 0-1.412-.587T15 20t.588-1.412T17 18t1.413.588T19 20t-.587 1.413T17 22M6.15 6l2.4 
             5h7l2.75-5zM5.2 4h14.75q.575 0 .875.513t.025 1.037l-3.55 6.4q-.275.5-.737.775T15.55 13H8.1L7 15h11q.425 0 .713.288T19
