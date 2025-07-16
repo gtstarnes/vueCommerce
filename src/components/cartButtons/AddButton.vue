cadc4ba177efd8c0cad644551e195dfbfb036089
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-07-16 07:38:11 -0400

    create add cart button

---
+++ src/components/cartButtons/AddButton.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+    import CartSvg from "../../assets/Cartsvg.vue"
+</script>
+
+<template>
+    <button>
+        <CartSvg />
+        Add to cart
+    </button>
+</template>
+
+
+<style lang="css" scoped>
+    button {
+        background-color: orangered;
+        font-weight: bold;
+        cursor: pointer;
+        border: none;
+        padding: 0.5em;
+        width: 100%;
+        border-radius: 4px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
